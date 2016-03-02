a484897d6e609e593b8786def64789989786f293
Fernando Daciuk <f.daciuk@gmail.com>
2016-03-01 23:43:09 -0300

    Fix code styling

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
-const inquirer = require('inquirer');
-const boleto = require('./lib/boleto');
+const inquirer = require('inquirer')
+const boleto = require('./lib/boleto')
 
 const twoDigits = (number) => {
-  return number < 10 ? '0' + number : number;
-};
+  return number < 10 ? '0' + number : number
+}
 
 const getDate = (date) => {
-  const day = twoDigits(date.getDate());
-  const month = twoDigits(date.getMonth() + 1);
-  return `${day}/${(month)}/${date.getFullYear()}`;
-};
+  const day = twoDigits(date.getDate())
+  const month = twoDigits(date.getMonth() + 1)
+  return `${day}/${(month)}/${date.getFullYear()}`
+}
 
 const questions = [
   {
     name: 'digits',
     type: 'input',
     message: 'Linha digitável:'
-  },{
+  }, {
     name: 'value',
     type: 'input',
     'default': '0,00',
     message: 'Valor (inclua os centavos, separando por vírgula):'
-  },{
+  }, {
     name: 'dueDate',
     type: 'input',
     'default': getDate(new Date()),
     message: 'Data do vencimento:'
   }
-];
+]
 
 inquirer.prompt(questions, (answers) => {
-  console.log('Nova linha digitável:', boleto(answers));
-});
+  console.log('Nova linha digitável:', boleto(answers))
+})
